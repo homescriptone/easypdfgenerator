359a0081d32405c5a5424f5628e70368da9219ef
Manu The Blacker <emmanueladekplovi1@gmail.Com>
2020-12-19 00:23:01 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+	public function boot(UrlGenerator $url)
+	{
+	    if (env('APP_ENV') !== 'local') {
+	        $url->forceScheme('https');
+	    }
+	}
+
     /**
      * Register any application services.
      *
