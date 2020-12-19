8da9df6ab342cfcf243b1f28916e75cf01f30233
Manu The Blacker <emmanueladekplovi1@gmail.Com>
2020-12-19 08:11:49 +0100

    Update CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -22,16 +22,17 @@ class CorsMiddleware
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
+        if ($request->isMethod('OPTIONS'))
+        {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
 
-        return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        $response = $next($request);
+        foreach($headers as $key => $value)
+        {
+            $response->header($key, $value);
+        }
 
-
-//        $response = $next($request);
-//        foreach($headers as $key => $value)
-//        {
-//            $response->header($key, $value);
-//        }
-//
-//        return $response;
+        return $response;
     }
 }
